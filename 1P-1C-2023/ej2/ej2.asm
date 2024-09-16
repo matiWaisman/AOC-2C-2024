d718d7c9a18440053b0759bfaa6a6a2fa45b48c4
Matias <matiasewaisman@gmail.com>
2024-09-16 20:10:28 -0300

    Added parciales

--- 1P-1C-2023/ej2/ej2.asm
+++ 1P-1C-2023/ej2/ej2.asm
@@ -7,6 +7,23 @@ section .text
                         ;    uint8_t *laCoincidencia[rcx] ){
 global miraQueCoincidencia
 miraQueCoincidencia:   
-    
+    ;prologo
+    ;prologo
+    push rbp
+    mov rbp, rsp ; Stack alíneado a 16 bytes
+    ; Hago rdx por rdx para que quede la cantidad total de píxeles
+    imul rdx, rdx
+    ; Uso a r8 como iterador
+    xor r8, r8
+    ciclo:
+        cmp r8, rdx
+        je epilogo
+        movdqa xmm0, [rdi] ; En xmm0 estan los 4 datos del píxel A actual
+        movdqa xmm1, [rsi] ; En xmm1 estan los 4 datos del píxel B actual
+
+    epilogo:
+        ;epilogo
+        pop rbp 
+        ret
     
 
