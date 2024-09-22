ac9b52e074012dc0829d1127923e78da6e668bf2
Matias <matiasewaisman@gmail.com>
2024-09-22 15:44:45 -0300

    Adedd recu segundo cuatri 2023

--- 1P-2C-2022/solucion/ej1/ej1.c
+++ 1P-2C-2022/solucion/ej1/ej1.c
@@ -1,4 +1,8 @@
 #include "ej1.h"
 
 char** agrupar_c(msg_t* msgArr, size_t msgArr_len){
+    char* res[msgArr_len]; // Declaro el array
+    for(uint64_t i = 0; i < msgArr_len; i++){ // Ciclo inicial para que todos sean el string vacío 
+        res[i] = "";
+    }
 }
